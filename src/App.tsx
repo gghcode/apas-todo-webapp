db0845d2693bbf98a16ba32666858c8eaaac90fc
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-12-20 10:59:30 +0000

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import { Login } from '@/pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,46 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 export const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const onLoginFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (username === '') {
+      alert('username is required');
+    }
+
+    if (password === '') {
+    }
+    console.log(username, password);
+  };
+
   return (
-    <div>
-      <div>
-        <form onSubmit={() => console.log('hello')}>
-          <input
-            placeholder="Please input username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Please input password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Login</button>
+    <div className="login-page">
+      <div className="login-form">
+        <h2 className="app-title-h2">APAS TODO</h2>
+        <form onSubmit={onLoginFormSubmit}>
+          <div className="form-group">
+            <input
+              placeholder="Please input username"
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-login-form form-control"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Please input password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-login-form form-control"
+            />
+          </div>
+
+          <button type="submit" className="btn-login-form btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     </div>
